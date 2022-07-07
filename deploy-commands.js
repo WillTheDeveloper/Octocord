14217c846bdbc0a993b845cc5495e8a77158300f
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-07-07 20:20:57 +0100

    Trying to resolve problem with deploy commands

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,5 +1,5 @@
-const { SlashCommandBuilder } = require('discordjs/builders');
-const { REST } = require('discordjs/rest');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const { clientId, guildId, token } = require('./config.json');
 
